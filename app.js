078bbae8bcde8dc863b4bbebc9c8594cfd8826ce
Mainak27 <rickmainakdas@gmail.com>
2024-07-26 00:58:03 +0530

    mandrin shop

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
+const expressSession = require("express-session");
+const flash = require("connect-flash");
+
 
 const db = require("./config/mongoose-connection");
+const indexRouter = require("./routes/index");
 const ownersRouter = require("./routes/ownersRouter");
 const usersRouter = require("./routes/usersRouter");
 const productsRouter = require("./routes/productsRouter");
+
+require("dotenv").config();
 
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-//const userModel = require("./models/user");
-//const postModel = require("./models/post");
-//const session = require('express-session');
-//const upload = require("./config/multerconfig");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -21,6 +23,16 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname,'public')));
 app.use(cookieParser());
 
+app.use(
+    expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+  })
+);
+app.use(flash());
+
+app.use("/", indexRouter);
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const {generateToken} = require("../utils/generateToken");
+const ownerModel = require("../models/owner-model"); 
 
-router.get("/",function(req,res){
-    res.send("hey it's working");
+if(process.env.NODE_ENV === "development"){
+    router.post("/create", async function(req,res){
+            let owners = await ownerModel.find();
+            if(owners.length >0) {
+                    req.flash("error","User Already Existing, Please Login.");
+                    return res.redirect("/create");
+            }
+
+            let{fullname,email,password}= req.body;
+
+            let createdOwner = await ownerModel.create({
+                fullname,
+                email,
+                password,
+            });
+            res.status(201).send(createdOwner);
+        });
+    }
+
+router.get("/admin",function(req,res){
+    let success = req.flash("success");
+    res.render("createproducts",{success});
 });
 
 module.exports = router;
